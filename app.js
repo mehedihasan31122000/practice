a658330019b60994e087c4ff890e73bc69cea7db
mehedihasan31122000 <mehedihasanmh5531735@gmail.com>
2022-10-20 11:54:15 +0600

    first commit

---
+++ app.js
@@ -0,0 +1,128 @@
+// const a = 15;
+
+
+// if (a == 2) {
+
+//     console.log("True")
+// }else if (a==5){
+//     console.log('value is 5');
+// }else{
+// console.log('False');
+// }
+
+// const fruit = 'papaya'
+
+// switch (fruit) {
+//     case 'papaya':
+//         console.log('Papayas price is 100 tk');
+//         break;
+//     case 'banana':
+//         console.log('Bananas price is 10 tk ');
+//         break;
+//     case 'watermelon':
+//         console.log('Watermelon price is 150 tk');
+//         break;
+
+//     default:
+//         console.log(`${fruit} Is not availabe`);
+//         break;
+// }
+
+// const ab = [5, 6, 7, 'mehedi', 9]
+// console.log(ab[3]);
+
+// console.log(ab[0]+ab[3]);
+// let arr=[]
+// let r = 1;
+// for (r; r <= 100; r=r+1) {
+//     arr.push(r)
+// }
+// console.log(arr);
+
+// let obj = {
+//     Name: 'Mehedi',
+//     Age: 22,
+//     Department: 'Computer',
+//     Address: 'Barishal'
+// }
+// console.log(obj);
+// console.log(obj.Address);
+
+
+
+
+
+// const car =['a','b','c','d','e']
+
+
+
+// const arr = []
+
+
+// for (let ab = 0; ab <= 50; ab++) {
+//     arr.push(ab)
+// }
+
+// const sum = arr.reduce((a, b) =>{
+//     return a + b
+// }, 0)
+
+
+// let arr = []
+// for (let k = 0; k <= 100; k++) {
+//     arr.push(k)
+// }
+
+// let sum = 0
+
+// arr.forEach(el => {
+//     sum = sum +el
+// });
+
+// console.log('sum is ' + sum);
+
+
+
+
+// const fruit = 'apple'
+// switch (fruit) {
+//     case "banana":
+//         console.log('banana price is 167 tk')
+//         break;
+//     case "apple":
+//         console.log('apple price is 333 tk')
+//         break;
+//     default:
+//         console.log('apple')
+//         break;
+// }
+
+
+
+
+
+
+
+// let arr=[]
+// let total=0
+// for (let i = 0; i<=100; i++){
+//     arr.push(i)
+//     total=total+i
+// }
+// console.log(total)
+// let arr = []
+// let sum=0
+// for (let m = 1; m <= 50; m++) {
+//     arr.push(m)
+//     sum=sum+m
+// }
+// console.log(sum);
+// console.log(arr);
+
+
+function abc(c,b,c) {
+    
+    c+b+c
+
+}
+console.log(abc());
